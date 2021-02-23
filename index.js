97d8364e08cccd8216fcf96d8e465b55f6e7f6ef
Laura Ramos <lauraramos.globant@latam.com>
2021-02-22 20:31:37 -0500

    creating server and routes for phonebook

---
+++ index.js
@@ -0,0 +1,84 @@
+const { response } = require('express')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.listen(3001, () => { console.log('Server running on port 3001') })
+
+const persons = [{
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456'
+},
+{
+    id: 2,
+    name: 'Laura Monsalve',
+    number: '040-1234445445'
+},
+{
+    id: 3,
+    name: 'Laura Ramos',
+    number: '040-144454456'
+}
+
+]
+
+app.get('/', (req, res) => {
+    res.send('Esta es la agenda de las Lauras')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const quantity = persons.length
+    let date = new Date()
+    res.send(`Phonebook has info for ${quantity} people <br> <br> ${date} `)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const { id } = req.params
+    const person = persons.find(person => person.id === Number(id))
+    if (person) { res.json(person) } else { res.status(404).end() }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const { id } = req.params
+    const personsFiltered = persons.filter(person => person.id !== Number(id))
+    // res.status(200).json(personsFiltered)
+    res.status(204).end()
+})
+
+app.post('/api/persons/', (req, res) => {
+    const payload = req.body;
+    if (!payload.name) {
+        res.status(400).send({ error: "missing name" })
+        return
+    }
+
+    if (!payload.number) {
+        res.status(400).send({ error: "missing number" })
+        return
+    }
+
+    if (persons.some((person) => person.name === payload.name)) {
+        res.status(409).send({ error: "name must be unique" })
+        return
+    }
+
+    const randomId = Math.floor(Math.random() * 1000)
+
+    const newPerson = {
+        id: randomId,
+        ...payload
+    }
+
+    persons.push(newPerson)
+
+    res.status(200).json(newPerson)
+
+    const { id } = randomId
+
+})
